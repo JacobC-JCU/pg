8367830b9e39c1fd1023a518b10d90108896c44d
JacobC-JCU <vavrij06@jcu.cz>
2024-11-10 01:38:06 +0100

    Update third.py

--- third.py
+++ third.py
@@ -1,5 +1,4 @@
-def je_prvocislo():
-    cislo = input("Zadej číslo: ")
+def je_prvocislo(cislo):
     trueFalse = 0
     cislo = int(cislo)
     odmocnina = cislo ** 0.5
@@ -15,23 +14,13 @@ def je_prvocislo():
     if trueFalse == 0:
         return True
 
-def vrat_prvocislo():
-    cislo = input("Zadej horní rozsah: ")
+def vrat_prvocislo(cislo):
     cislo = int(cislo)
-
     scale_list = range(2, cislo + 1)
     seznam_Cisel = []
 
-
     for number in scale_list:
-        prvocislo = True
-
-        for odmocnitel in range(2, int(number ** 0.5) + 1):
-            if number % odmocnitel == 0:
-                prvocislo = False
-                continue
-
-        if prvocislo:
+        if je_prvocislo(number):
             seznam_Cisel.append(number)
 
     return seznam_Cisel
@@ -43,13 +32,14 @@ if __name__ == "__main__":
     menu = input("Zvolte možnost: ")
 
     if menu == "1":
-        vysledek = je_prvocislo()
+        cislo = input("Zadej číslo: ")
+        vysledek = je_prvocislo(cislo)
         if vysledek:
             print("Ćíslo je prvočíslo.")
         else:
             print("Číslo není prvočíslo.")
     elif menu == "2":
-        print(vrat_prvocislo())
+        cislo = input("Zadej horní hranici: ")
+        print(vrat_prvocislo(cislo))
     else:
         print("Chybný formát odpovědi..")
-
